c6ae36d00b9070e55d5090af22343aa7a7639295
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-07 15:02:10 +0400

    Setter and getter for parameters

--- Item.java
+++ Item.java
@@ -7,5 +7,36 @@ public class Item {
     private Float price;
     private Integer quantity;
 
-    
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
