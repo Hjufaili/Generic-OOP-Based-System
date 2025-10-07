457da28a16f6ae4c26120418a1170896446f7354
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-07 15:06:40 +0400

    Show Item Menu

---
+++ MainDriver.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainDriver {
+
+
+
+    public static void showItemMenu() {
+        System.out.println("""
+                =====Item Management Menu=====
+                1- Add a new item
+                2- Edit an Existing Item
+                3- Remove an Item
+                4- Display All Items
+                5- Exit
+                ==============================
+                Please enter your choice:
+                """);
+    }
+
+
+
+
+}
