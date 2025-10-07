b341e67bc07167a1018dc2bbc9271d8047b03098
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-07 15:10:47 +0400

    Add New Item

--- MainDriver.java
+++ MainDriver.java
@@ -11,6 +11,10 @@ public class MainDriver {
     public static void main(String[] args) {
         while (itemOption != 5) {
             showItemMenu();
+            itemOption = scanner.nextInt();
+            if (itemOption == 1) {
+                addNewItem();
+            }
 
         }
     }
@@ -27,6 +31,37 @@ public class MainDriver {
                 ==============================
                 Please enter your choice:
                 """);
+    }
+    public static void addNewItem() {
+        System.out.println("Add a new item");
+        boolean flag = true;
+        while (flag) {
+            Item item = new Item();
+            System.out.println("Enter the item id");
+            Integer itemId = scanner.nextInt();
+            while (itemId == null || itemId < 0 || checkIfItemIdExists(itemId)) {
+                System.out.println("Input is not accepted, please enter another ID");
+                itemId = scanner.nextInt();
+            }
+            item.setId(itemId);
+            scanner.nextLine();
+            System.out.println("Enter item name");
+            String itemName = scanner.nextLine();
+            item.setName(itemName);
+
+            System.out.println("Enter item price");
+            float itemPrice = scanner.nextFloat();
+            scanner.nextLine();
+            item.setPrice(itemPrice);
+
+            itemsList.add(item);
+            System.out.println("The item successfully added");
+            System.out.println("Do you want to exit press (q) or press anything else to cont. !");
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("q")) {
+                flag = false;
+            }
+        }
     }
 
 
