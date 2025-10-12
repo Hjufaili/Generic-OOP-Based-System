cced43e8777c1f48c0004bd8a44af1b34bec278b
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-12 14:44:47 +0400

    Add OfficeFurniture class

---
+++ OfficeFurniture.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class OfficeFurniture extends FurnitureItem{
+
+    private Float ergonomicRating;
+    private List<String> adjustabilityFeatures;
+    private Float weightCapacity;
+
+}
